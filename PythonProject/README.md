5d42305477f95c20c4f88fe7f233b187c2400b16
Stusio <156315994+StuOnGit@users.noreply.github.com>
2024-12-31 16:07:21 +0100

    Refactor phase

---
+++ PythonProject/README.md
@@ -0,0 +1,20 @@
+python3 -m venv .venv
+
+Su Windows
+
+.\.venv\Scripts\Activate
+
+pip install -r requirements.txt
+
+
+
+Su MacOS\Linux
+
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+
+
+----------
+and then usage.
--- sketch_nov19a/sketch_nov19a.ino
+++ sketch_nov19a/sketch_nov19a.ino
@@ -2,12 +2,16 @@
 #include <SPI.h>
 #include <ArduinoECCX08.h>
 #include <EEPROM.h>
+#include <AESLib.h>
 
 #define SIGNATURE_ADDRESS 0  // Indirizzo della firma
 #define SIGNATURE_VALUE 0xA5 // Valore della firma
 #define PIN_RESET 7 // Pin a cui è collegato il PULSTANTE RESET(TE)
+#define PIN_RED 6 // Pin del led rosso
+#define AES_BLOCKLEN 16 
 
-const unsigned int MAX_LENGTH = 32;
+
+const unsigned int MAX_LENGTH = 128;
 //TIM-88839994, d4U7hf5kDUKt6ThHud9RHuCQ
 char ssid[MAX_LENGTH];
 char password[MAX_LENGTH];
@@ -15,18 +19,22 @@ char encrypted_ssid_and_pass[MAX_LENGTH];
 char key[MAX_LENGTH];
 // char pin[6]; // PIN per autorizzare l'accesso
 int status = WL_IDLE_STATUS;
+int resetState = 0;
 
 int connectionStatus = WL_IDLE_STATUS;
 unsigned int triesToConnection = 3;
+
 
 void clearEncryptionData(){
   clearEncryptedCredentialsSignature();
   clearEncryptedCredentials();
 }
 
+
 void clearEncryptedCredentialsSignature() {
   EEPROM.write(SIGNATURE_ADDRESS, 0xFF); // Resetta la firma
 }
+
 
 void clearEncryptedCredentials() {
   for(int i = 0; i < sizeof(encrypted_ssid_and_pass); i++) {
@@ -34,17 +42,16 @@ void clearEncryptedCredentials() {
   }
 }
 
+
 bool encryptedCredentialsAreSaved() {
   return EEPROM.read(SIGNATURE_ADDRESS) == SIGNATURE_VALUE;
 }
+
 
 void saveEncryptedCredentialsSignature() {
   EEPROM.write(SIGNATURE_ADDRESS, SIGNATURE_VALUE);
 }
 
-void clearEncryptedCredentialsSignature() {
-  EEPROM.write(SIGNATURE_ADDRESS, 0xFF); // Resetta la firma
-}
 
 // TODO: da cambiare in booleano o stringa [vedere se è sicuro] per gestione di errore
 void requestWifiCredentials(){
@@ -56,10 +63,12 @@ void requestWifiCredentials(){
     secureReadInput(password, sizeof(password));
 }
 
+
 void requestEncryptedWifiAndPassword(){
   Serial.println("Encrypted SSID_and_Password:");
   secureReadInput(encrypted_ssid_and_pass, sizeof(encrypted_ssid_and_pass));
 }
+
 
 void requestKey(){
   Serial.println("KEY:");
@@ -68,7 +77,7 @@ void requestKey(){
 
 
 void saveEncryptedCredentialsToEEPROM(){
-  Serial.println("Saving encripted credentials..");
+  Serial.println("Starting the saving encription credentials for WIFI..");
   requestEncryptedWifiAndPassword();
 
   int dataAddress = SIGNATURE_ADDRESS + 1; 
@@ -77,6 +86,44 @@ void saveEncryptedCredentialsToEEPROM(){
   EEPROM.put(dataAddress, encrypted_ssid_and_pass);
   saveEncryptedCredentialsSignature(); // Salva la firma
   Serial.println("Credentials Saved");
+}
+
+
+void hexStringToByteArray(const char *hexString, byte *byteArray, size_t arraySize) {
+  for (size_t i = 0; i < arraySize; i++) {
+    sscanf(&hexString[i * 2], "%2hhx", &byteArray[i]);
+  }
+}
+
+
+void decryptAES(const char* encryptedHex, const char* keyHex, char* out){
+  byte aesKey[MAX_LENGTH];  
+  byte encryptedData[MAX_LENGTH];
+  byte iv[AES_BLOCKLEN]; // Vettore di inizializzazione
+
+  // Converti i dati esadecimali in array di byte
+  hexStringToByteArray(keyHex, aesKey, MAX_LENGTH);
+  hexStringToByteArray(encryptedHex, encryptedData, MAX_LENGTH);
+
+  // Estrai IV e messaggio cifrato
+  memcpy(iv, encryptedData, AES_BLOCKLEN); // I primi 16 byte sono l'IV
+  byte ciphertext[MAX_LENGTH - AES_BLOCKLEN];
+  memcpy(ciphertext, encryptedData + AES_BLOCKLEN, MAX_LENGTH - AES_BLOCKLEN);
+
+  // Configura AES
+  AESLib aesLib;
+  aesLib.gen_iv(iv); // Usa il vettore di inizializzazione estratto
+  aesLib.set_paddingmode((paddingMode)0); // Nessun padding (adatta al tuo schema)
+
+  // Decripta
+  aesLib.decrypt((char *)ciphertext, out, MAX_LENGTH - AES_BLOCKLEN, (char *)aesKey, MAX_LENGTH, iv);
+  out[MAX_LENGTH - AES_BLOCKLEN] = '\0'; // Aggiungi il terminatore
+
+}
+
+
+void decriptedToCredentials(){ // the standard is 'SSID,PASSWORD'
+
 }
 
 
@@ -110,14 +157,18 @@ void connectToWiFi() {
   // prendi il cifrato e decifralo con la chiave
   // inserisci quindi in ssid e password e connettiti
 
+/*
   status = WiFi.begin(ssid, password);
-  while(status != WL_CONNECTED){
-    delay(2000);
+  int timerExit = 4;
+  while(status != WL_CONNECTED || timerExit == 0){
+    delay(1000);
     Serial.println("...");
+    timerExit--;
   }
   Serial.println("\nConnesso!");
   //DEBUG
   Serial.println(WiFi.localIP());
+  */
 }
 
 /***
@@ -148,24 +199,24 @@ void initializeSecureElement(){
 
 void setup() {
   Serial.begin(9600);
+  pinMode(PIN_RED, OUTPUT);
   pinMode(PIN_RESET, INPUT_PULLUP);
   while(!Serial){}
-  if (digitalRead(PIN_RESET) == LOW) {
+  if (digitalRead(PIN_RESET) == HIGH) {
+    digitalWrite (PIN_RED, HIGH);
     Serial.println("Resetting.. Data bye bye");
     clearEncryptionData();
     Serial.println("Data gone..");
+    delay(4000);
+    digitalWrite (PIN_RED, LOW);
   } else {
-    Serial.println("Normal Start..");
+    Serial.print("Normal ");
   }
-  initializeSecureElement();
-  //requestWifiCredentials();
-  //se va a buon fine
+  Serial.println("Starting..");
+ 
   connectToWiFi();
-  //se va a buon fine tutto a posto!
-
 }
 void loop() {
-
-  delay(1000);
+  delay(100);
 }
 
